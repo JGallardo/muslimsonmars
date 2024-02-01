c37e4c32ca006ebcfbaa93875e800331e7bcdf87
Juan <jgallardo720@gmail.com>
2024-01-31 21:41:50 -0800

    initial commit. Minor text edits.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Muslims on Mars`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Juan Gallardo`,
+      summary: `Future forward Muslim.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `jgallardo949`,
     },
   },
   plugins: [
